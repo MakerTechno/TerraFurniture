b287f8012b58732167fd490664aa0e7ef4810b7f
westernat <westernat233@163.com>
2025-01-15 13:08:08 +0800

    把家具分支纳入本体

---
+++ src/main/java/nowebsite/makertechno/terra_furniture/TerraFurniture.java
@@ -0,0 +1,17 @@
+package nowebsite.makertechno.terra_furniture;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Mod(TerraFurniture.MODID)
+public class TerraFurniture {
+    public static final String MODID = "terra_furniture";
+    private static final Logger LOGGER = LoggerFactory.getLogger("Terra Furniture");
+
+    public TerraFurniture(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+}
