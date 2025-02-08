2df40451ed6434b65cde0d5e30bd1c86930990ad
westernat <westernat233@163.com>
2025-02-08 12:01:03 +0800

    调整创造模式标签页

--- src/main/java/nowebsite/makertechno/terra_furniture/common/init/TFTabs.java
+++ src/main/java/nowebsite/makertechno/terra_furniture/common/init/TFTabs.java
@@ -3,15 +3,14 @@ package nowebsite.makertechno.terra_furniture.common.init;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import nowebsite.makertechno.terra_furniture.TerraFurniture;
-
-import java.util.function.Supplier;
 
 public final class TFTabs {
     public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(BuiltInRegistries.CREATIVE_MODE_TAB, TerraFurniture.MODID);
 
-    public static final Supplier<CreativeModeTab> FURNITURE = TABS.register("furniture", () -> CreativeModeTab.builder().icon(TFBlocks.PLASTIC_CHAIR::toStack)
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> FURNITURE = TABS.register("furniture", () -> CreativeModeTab.builder().icon(TFBlocks.PLASTIC_CHAIR::toStack)
             .title(Component.translatable("creativetab.terra_furniture"))
             .displayItems((parameters, output) -> {
                 TFItems.ITEMS.getEntries().forEach(item -> output.accept(item.get()));
