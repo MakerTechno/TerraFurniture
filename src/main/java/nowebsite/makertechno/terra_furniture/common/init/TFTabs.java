59c4c35b34f0122157c78251918b784a8b5295e6
westernat <westernat233@163.com>
2025-03-14 23:10:52 +0800

    创造模式标签页进行双向绑定

--- src/main/java/nowebsite/makertechno/terra_furniture/common/init/TFTabs.java
+++ src/main/java/nowebsite/makertechno/terra_furniture/common/init/TFTabs.java
@@ -1,7 +1,10 @@
 package nowebsite.makertechno.terra_furniture.common.init;
 
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -15,6 +18,9 @@ public final class TFTabs {
             .displayItems((parameters, output) -> {
                 TFItems.ITEMS.getEntries().forEach(item -> output.accept(item.get()));
                 TFBlocks.BLOCKS.getEntries().forEach(block -> output.accept(block.get()));
-            }).build());
+            })
+            .withTabsAfter(ResourceKey.create(Registries.CREATIVE_MODE_TAB, ResourceLocation.fromNamespaceAndPath("confluence", "mechanical")))
+            .withTabsBefore(ResourceKey.create(Registries.CREATIVE_MODE_TAB, ResourceLocation.fromNamespaceAndPath("confluence", "building_blocks")))
+            .build());
 
 }
