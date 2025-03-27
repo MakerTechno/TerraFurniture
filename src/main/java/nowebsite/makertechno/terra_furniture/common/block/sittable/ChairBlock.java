4d63eb0b3007d47a7bc4f32200d48ba968d3f473
MakerTechno <stevenyth@163.com>
2025-03-27 22:55:31 +0800

    清除冗余import

--- src/main/java/nowebsite/makertechno/terra_furniture/common/block/sittable/ChairBlock.java
+++ src/main/java/nowebsite/makertechno/terra_furniture/common/block/sittable/ChairBlock.java
@@ -14,7 +14,6 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
