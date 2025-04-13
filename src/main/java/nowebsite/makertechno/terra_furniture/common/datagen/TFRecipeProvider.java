6cb4ce9bb9e3c8226121419fc0eba80e19e36c1e
MagicHarp <161911368+MagicHarp@users.noreply.github.com>
2025-04-13 20:50:42 +0800

    加再生法杖，地牢楼梯台阶的json相关，玻璃窑对应合成

--- src/main/java/nowebsite/makertechno/terra_furniture/common/datagen/TFRecipeProvider.java
+++ src/main/java/nowebsite/makertechno/terra_furniture/common/datagen/TFRecipeProvider.java
@@ -1,9 +1,11 @@
 package nowebsite.makertechno.terra_furniture.common.datagen;
 
+import net.minecraft.client.resources.model.Material;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -12,6 +14,7 @@ import nowebsite.makertechno.terra_furniture.TerraFurniture;
 import nowebsite.makertechno.terra_furniture.common.init.TFBlocks;
 import nowebsite.makertechno.terra_furniture.common.recipe.GlassKilnRecipe;
 import org.confluence.lib.common.data.gen.AbstractRecipeProvider;
+import org.confluence.lib.common.recipe.AmountIngredient;
 
 import java.util.List;
 import java.util.Map;
@@ -31,10 +34,86 @@ public class TFRecipeProvider extends AbstractRecipeProvider {
                 "GG",
                 "GG"
         )), 0.1F, 200);
+        recipe(recipeOutput, TFBlocks.GLASS_CHAIR.toStack(), ShapedRecipePattern.of(Map.of(
+                'G', Ingredient.of(Items.GLASS)
+        ), List.of(
+                "G ",
+                "GG"
+        )), 0.1F, 200);
+        recipe(recipeOutput, TFBlocks.GLASS_TOILET.toStack(), ShapedRecipePattern.of(Map.of(
+                'G', Ingredient.of(Items.GLASS)
+        ), List.of(
+                "GG ",
+                " GG",
+                " GG"
+        )), 0.1F, 200);
+        recipe(recipeOutput, TFBlocks.GLASS_TABLE.toStack(), ShapedRecipePattern.of(Map.of(
+                'G', Ingredient.of(Items.GLASS),
+                'A', AmountIngredient.of(2,Items.GLASS)
+        ), List.of(
+                "GGGG",
+                " AA "
+        )), 0.1F, 200);
+        recipe(recipeOutput, TFBlocks.GLASS_SOFA.toStack(), ShapedRecipePattern.of(Map.of(
+                'G', Ingredient.of(Items.GLASS),
+                'A', Ingredient.of(ItemTags.WOOL)
+        ), List.of(
+                "GAAG",
+                "GGGG"
+        )), 0.1F, 200);
+        recipe(recipeOutput, TFBlocks.GLASS_CHANDELIER.toStack(), ShapedRecipePattern.of(Map.of(
+                'G', Ingredient.of(Items.GLASS),
+                'A', Ingredient.of(Items.CHAIN),
+                'B', AmountIngredient.of(4,Items.TORCH)
+        ), List.of(
+                " A ",
+                "GBG",
+                " G "
+        )), 0.1F, 200);
+        recipe(recipeOutput, TFBlocks.GLASS_SINK.toStack(), ShapedRecipePattern.of(Map.of(
+                'G', Ingredient.of(Items.GLASS),
+                'A', Ingredient.of(Items.WATER_BUCKET)
+        ), List.of(
+                "GAG",
+                "GG "
+        )), 0.1F, 200);
+        recipe(recipeOutput, TFBlocks.GLASS_CANDLE.toStack(), ShapedRecipePattern.of(Map.of(
+                'G', Ingredient.of(Items.GLASS),
+                'A', Ingredient.of(Items.TORCH)
+        ), List.of(
+                " G ",
+                "GAG"
+        )), 0.1F, 200);
+        recipe(recipeOutput, TFBlocks.GLASS_LANTERN.toStack(), ShapedRecipePattern.of(Map.of(
+                'G', Ingredient.of(Items.GLASS),
+                'A', Ingredient.of(Items.TORCH)
+        ), List.of(
+                " G ",
+                "GAG",
+                "GGG"
+        )), 0.1F, 200);
+        recipe(recipeOutput, TFBlocks.GLASS_LAMP.toStack(), ShapedRecipePattern.of(Map.of(
+                'G', Ingredient.of(Items.GLASS),
+                'A', Ingredient.of(Items.TORCH)
+        ), List.of(
+                " A ",
+                " G ",
+                " G ",
+                " G "
+        )), 0.1F, 200);
+        recipe(recipeOutput, TFBlocks.GLASS_CANDELABRAS.toStack(), ShapedRecipePattern.of(Map.of(
+                'G', Ingredient.of(Items.GLASS),
+                'A', AmountIngredient.of(2,Items.TORCH)
+        ), List.of(
+                "AGA",
+                "GGG",
+                " G "
+        )), 0.1F, 200);
     }
 
     protected void recipe(RecipeOutput recipeOutput, ItemStack result, ShapedRecipePattern pattern, float experience, int cookingTime) {
         ResourceLocation id = TerraFurniture.asResource(getItemName(result.getItem()));
-        recipeOutput.accept(id, new GlassKilnRecipe(TFBlocks.GLASS_DOOR.toStack(), pattern, experience, cookingTime), null);
+        recipeOutput.accept(id, new GlassKilnRecipe(result, pattern, experience, cookingTime), null);
+
     }
 }
