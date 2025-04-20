e263ea56020882582a9693e2bf60fe2d969fe738
Ironnoob73 <67966070+Ironnoob73@users.noreply.github.com>
2025-04-20 13:42:37 +0800

    Update TFLootTableProvider.java

--- src/main/java/org/confluence/terra_furniture/common/datagen/TFLootTableProvider.java
+++ src/main/java/org/confluence/terra_furniture/common/datagen/TFLootTableProvider.java
@@ -6,9 +6,11 @@ import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DoorBlock;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import org.confluence.terra_furniture.common.init.TFBlocks;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Set;
@@ -23,17 +25,27 @@ public class TFLootTableProvider extends LootTableProvider {
     }
 
     public static class BlockSub extends BlockLootSubProvider {
+        List<Block> blockList = TFBlocks.BLOCKS.getEntries()
+                .stream()
+                .map(e -> (Block) e.value())
+                .toList();
         protected BlockSub(HolderLookup.Provider provider) {
             super(Set.of(), FeatureFlags.REGISTRY.allFlags(), provider);
         }
 
         @Override
         protected void generate() {
-            TFBlocks.BLOCKS.getEntries().forEach(block -> dropSelf(block.get()));
+            for (Block block : blockList) {
+                if (!(block instanceof DoorBlock)){
+                    dropSelf(block);
+                } else {
+                    map.put(block.getLootTable(),createDoorTable(block));
+                }
+            }
         }
 
         @Override
-        protected Iterable<Block> getKnownBlocks() {
+        protected @NotNull Iterable<Block> getKnownBlocks() {
             return TFBlocks.BLOCKS.getEntries().stream().map(DeferredHolder::get).collect(Collectors.toUnmodifiableList());
         }
     }
