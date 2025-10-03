f9adffebd65929797b443887ce530f33255e2293
MagicHarp <161911368+MagicHarp@users.noreply.github.com>
2025-10-03 23:03:50 +0800

    补MC原版的物品价值

--- src/main/java/org/confluence/terra_furniture/common/datagen/TFChineseProvider.java
+++ src/main/java/org/confluence/terra_furniture/common/datagen/TFChineseProvider.java
@@ -36,6 +36,7 @@ public class TFChineseProvider extends LanguageProvider {
         add(TFBlocks.PUPFISH_BOWL.get(), "鳉鱼缸");
         add(TFBlocks.LAVA_SERPENT_BOWL.get(), "熔岩蛇缸");
         add(TFBlocks.TRASH_CAN.get(), "垃圾桶");
+        add(TFBlocks.HANGING_POT.get(), "吊挂盆");
 
         add(TFBlocks.PLASTIC_CHAIR.get(), "塑料椅");
         add(TFBlocks.GLASS_DOOR.get(), "玻璃门");
