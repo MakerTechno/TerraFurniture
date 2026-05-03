324e2b74220fee359eedfc378e1190196541c2ef
westernat <westernat233@163.com>
2026-05-03 23:04:15 +0800

    重构

--- src/main/java/org/confluence/terra_furniture/common/recipe/GlassKilnRecipe.java
+++ src/main/java/org/confluence/terra_furniture/common/recipe/GlassKilnRecipe.java
@@ -11,7 +11,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.level.Level;
 import org.confluence.lib.common.recipe.AbstractAmountRecipe;
-import org.confluence.lib.mixed.LibShapedRecipePattern;
+import org.confluence.lib.mixed.ILibShapedRecipePattern;
 import org.confluence.terra_furniture.common.init.TFBlocks;
 import org.confluence.terra_furniture.common.init.TFRegistries;
 import org.jetbrains.annotations.Nullable;
@@ -26,7 +26,7 @@ public class GlassKilnRecipe extends AbstractAmountRecipe<CraftingInput> {
         this.pattern = pattern;
         this.experience = experience;
         this.cookingTime = cookingTime;
-        LibShapedRecipePattern.setNonSymmetricalMatching(pattern);
+        ILibShapedRecipePattern.setNonSymmetricalMatching(pattern);
     }
 
     public float getExperience() {
--- src/main/java/org/confluence/terra_furniture/references/TFConfluenceRefs.java
+++ src/main/java/org/confluence/terra_furniture/references/TFConfluenceRefs.java
@@ -2,10 +2,10 @@ package org.confluence.terra_furniture.references;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import org.apache.logging.log4j.util.TriConsumer;
+import org.confluence.lib.util.consumer.Consumer3;
 import org.confluence.terra_furniture.common.entity.RideableEntityNull;
 
 public class TFConfluenceRefs {
-    public static TriConsumer<RideableEntityNull, ServerLevel, BlockPos> poop_task =
+    public static Consumer3<RideableEntityNull, ServerLevel, BlockPos> poop_task =
             (entityNull, serverLevel, pos) -> {};
 }
